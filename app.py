0214f76c8328ea6bb66074a7c3c40d473bf26587
boreddomed <mrana.bese15seecs@seecs.edu.pk>
2017-08-07 13:16:23 +0500

    3.4

--- app.py
+++ app.py
@@ -43,8 +43,10 @@ def processRequest(req):
 
     #baseurl = "https://aarz.pk/bot/index.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+latest+"&UnitArea="+area_property+"&Unit="+unit_property+"&school="+school+"&airport="+airport+"&transport="+transport+"&security="+security+"&shopping_mall="+malls+"&fuel="+fuel
     #baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&price_min="+maximum_value+"&price_max=0estate_agent=&purpose=Sell&property_type="+property_type
-
-    baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&type="+property_type+"&price_max="+maximum_value+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
+    if maximum_value == 0:
+        baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&type="+property_type+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
+    else:  
+        baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&type="+property_type+"&price_max="+maximum_value+"&land_area="+unit_property+"&min_r=0&max_r="+max_area
     #print("city:",city_names)
     print("url is:",baseurl)
     result = urllib.request.urlopen(baseurl).read()
@@ -112,19 +114,16 @@ def processProjectName(req):
 
 #Price
 def convertMaximumIfWords(req):
-    if req[0] == '' or req[0] == ' ':
-        return 0
-    else:
-        price, unitt = req.split()
-        price = int(price)
-        if unitt[0] == 'l' or unitt[0] == 'L':
-            price = price * (10 ** 5)
-        elif unitt[0] == 'm' or unitt[0] == 'M':
-            price = price * (10 ** 6)
-        elif unitt[0] == 'c' or unitt[0] == 'C':
-            price = price * (10 ** 7)
-        print(price)
-        return str(price)
+    price, unitt = req.split()
+    price = int(price)
+    if unitt[0] == 'l' or unitt[0] == 'L':
+        price = price * (10 ** 5)
+    elif unitt[0] == 'm' or unitt[0] == 'M':
+        price = price * (10 ** 6)
+    elif unitt[0] == 'c' or unitt[0] == 'C':
+        price = price * (10 ** 7)
+    print(price)
+    return str(price)
 
 def convertMaximumIfNumber(req):
     price=int(req)
